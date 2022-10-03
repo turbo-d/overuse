29582f0a6c3ebc833fceeabe4626d877cbd60be4
Dale Luginbuhl <dale.luginbuhl@gmail.com>
2022-10-03 11:51:28 -0400

    Add pgadmin to docker-compose and update table creation queries

--- db/migrations/000002_create_entries_table.up.sql
+++ db/migrations/000002_create_entries_table.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS entries(
-    id INT GENERATED ALWAYS AS IDENTITY,
+    entry_id INT GENERATED ALWAYS AS IDENTITY,
     activity_id INT NOT NULL,
     occured_on DATE NOT NULL CHECK (occured_on <= CURRENT_DATE),
     volume INT NOT NULL CHECK (volume > 0),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (id)
+    PRIMARY KEY (entry_id),
     FOREIGN KEY (activity_id)
-        REFERENCES activities (id)
+        REFERENCES activities (activity_id)
         ON DELETE CASCADE
 );
